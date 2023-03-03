cc831287ce6132cbd9fda42567f0bb7f6df94476
SPoluliakh <S_Poluliakh@ukr.net>
2023-03-03 11:43:02 +0200

    update

--- middlewars/validation.js
+++ middlewars/validation.js
@@ -1,6 +1,13 @@
 const validation = (schema) => {
   return (req, res, next) => {
     const { body } = req;
+
+    if (Object.getOwnPropertyNames(body).length === 0) {
+      const err = new Error("body is missing");
+      err.status = 400;
+      next(err);
+    }
+
     const { error } = schema.validate(body);
     if (error) {
       error.status = 400;
--- schemas/contacts.js
+++ schemas/contacts.js
@@ -1,9 +1,15 @@
 const Joi = require("joi");
 
 const schema = Joi.object({
-  name: Joi.string().required(),
-  email: Joi.string().email().required(),
-  phone: Joi.string().required(),
+  name: Joi.string().required().messages({
+    "any.required": "Поле name является обязательным",
+  }),
+  email: Joi.string().email().required().messages({
+    "any.required": "Поле email является обязательным",
+  }),
+  phone: Joi.string().required().messages({
+    "any.required": "Поле phone является обязательным",
+  }),
 });
 
 module.exports = schema;
